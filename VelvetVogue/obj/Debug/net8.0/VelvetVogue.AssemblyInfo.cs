5844f11a0c4bca873dc21b0d1114065a7f89442a
Ravidu Sandesh <sandeshpiumantha52@gmail.com>
2025-10-15 18:48:57 +0530

    home page modification

--- VelvetVogue/obj/Debug/net8.0/VelvetVogue.AssemblyInfo.cs
+++ VelvetVogue/obj/Debug/net8.0/VelvetVogue.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VelvetVogue")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c947e735a999d0e7ba76ae6db36e189bbebc86ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ae48121ef6f45533eeb252d387cf19d18555342")]
 [assembly: System.Reflection.AssemblyProductAttribute("VelvetVogue")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VelvetVogue")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
