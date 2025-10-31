f277e5f743564494230e52dfa09aa2dc5e07e71d
Ravidu Sandesh <sandeshpiumantha52@gmail.com>
2025-10-31 10:52:22 +0530

    Task: Creating Add login page & add modification in nav bar | status: complete

---
+++ VelvetVogue/Controllers/AddNewLoginController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VelvetVogue.Models.ViewModels;
+
+namespace VelvetVogue.Controllers
+{
+    public class AddNewLoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new AddNewLoginViewModel());
+        }
+    }
+}
